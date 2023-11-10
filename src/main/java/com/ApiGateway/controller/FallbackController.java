140f5d83d6a1574661e59727f29b3265e877a4a1
Ayushver <vermaayu793@gmail.com>
2023-11-11 00:15:19 +0530

    apiGateway

---
+++ src/main/java/com/ApiGateway/controller/FallbackController.java
@@ -0,0 +1,18 @@
+package com.ApiGateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/userServiceFallback")
+    public String userServiceFallback(){
+        return "User service is down at this time !!";
+    }
+
+    @GetMapping("?contactServiceFallback")
+    public String contactServiceFallback(){
+        return "Contact Service is down at this time";
+    }
+}
